8ff6d7ccece1a0874dee42be5183260c66e18e7c
Yasu Flores <carlosyasu91@gmail.com>
2017-12-14 19:35:36 -0800

    old login with fb

--- src/features/login/LogIn.js
+++ src/features/login/LogIn.js
@@ -3,14 +3,20 @@ import * as FontAwesome from 'react-icons/lib/fa'
 
 import './login.css'
 
+import { signInWithFacebook } from 'utils/firebase/auth'
+
 const LogIn = ({ openFacebookOAuth}) => { 
   return (
     <div>
-      <a href="http://localhost:8080/auth/facebook">
-      <button className="fb-login">
+      {
+        //<a href="http://localhost:8080/auth/facebook">
+      }
+      <button className="fb-login" onClick={ signInWithFacebook }>
         <FontAwesome.FaFacebookSquare className="fb-logo" /> Log in with Facebook
       </button>
-      </a>
+      {
+        // </a>
+      }
     </div>
   )
 }
